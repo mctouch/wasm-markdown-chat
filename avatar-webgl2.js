026282c4758b2ce84bc75b2fdf22fb3566af9d39
Developer <dev@example.com>
2026-05-11 00:16:24 +0000

    fix(webgl2): preserveDrawingBuffer true for Safari compositing

--- avatar-webgl2.js
+++ avatar-webgl2.js
@@ -6,11 +6,11 @@
 class WebGL2AvatarRenderer {
     constructor(canvas) {
         this.canvas = canvas;
-        const gl = canvas.getContext('webgl2', {
+        this.gl = canvas.getContext('webgl2', {
             antialias: true,
             alpha: true,
             premultipliedAlpha: false,
-            preserveDrawingBuffer: false
+            preserveDrawingBuffer: true
         });
         if (!gl) throw new Error('WebGL2 not available');
         this.gl = gl;
@@ -351,9 +351,9 @@ class WebGL2AvatarRenderer {
         const mv = this.multiply(view, model); // standard OpenGL: view * model
         const nm = this.normalMatrix(mv);
 
-        gl.uniformMatrix4fv(this.uloc.p, true, proj);
-        gl.uniformMatrix4fv(this.uloc.mv, true, mv);
-        gl.uniformMatrix3fv(this.uloc.n, true, nm);
+        gl.uniformMatrix4fv(this.uloc.p, false, proj);
+        gl.uniformMatrix4fv(this.uloc.mv, false, mv);
+        gl.uniformMatrix3fv(this.uloc.n, false, nm);
         gl.uniform3f(this.uloc.bc, 1, 1, 1);
 
         // Draw all primitives
